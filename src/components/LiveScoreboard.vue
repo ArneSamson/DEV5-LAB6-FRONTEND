cd0f67fac054e1a20cba1c8b0a4c6efd7629759c
ArneSamson <arnesamson007@gmail.com>
2023-11-24 15:28:53 +0100

    feat: add basic logic for webSocket

--- src/components/LiveScoreboard.vue
+++ src/components/LiveScoreboard.vue
@@ -1,10 +1,27 @@
 <script>
+    import {ref, onMounted} from 'vue';
 
+    let score = ref(0);
+    let team = ref('home');
+
+    let socket = null;
+
+    onMounted(() => {
+        socket = new WebSocket('ws://localhost:3000');
+
+        socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            score.value = data.score;
+            team.value = data.team;
+
+            console.log(data);
+        };
+    });
 </script>
 
 <template>
   <div>
-
+    
   </div>
 
 </template>
